9d9b9cb49f869b039cdccb5f602a397e7800a874
Naoki Horie <jp7eph@gmail.com>
2022-06-25 03:08:36 +0900

    major release (#3)

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type Hex int
+
+func (h Hex) String() string {
+	return fmt.Sprintf("0x%x", int(h))
+}
+
 func main() {
 	fmt.Println("hello, world")
+	var h Hex
+	h = 2
+	fmt.Println(h.String())
 }
